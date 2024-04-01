45508fbe7865d7bf01e1575bbfba71998e47a2a3
deankago <martokhago@gmail.com>
2024-04-01 14:58:10 +0300

    add home screen and tabbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import {NavigationProp, RouteProp} from '@react-navigation/native';
+import {
+  NavigationProp,
+  NavigatorScreenParams,
+  RouteProp,
+} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import signUp from '../screens/SignUp';
@@ -8,13 +12,28 @@ import checkOut from '../screens/CheckOut';
 import details from '../screens/Details';
 import getStarted from '../screens/GetStarted';
 import logIn from '../screens/LogIn';
-import myProfile from '../screens/MyProfile';
 import orderConfirmationRider from '../screens/OrderConfirmationRider';
 import orderHistory from '../screens/OrderHistory';
+import {useKeyboard} from '@react-native-community/hooks';
+import {Platform, StatusBar, View} from 'react-native';
+import {
+  BottomTabBar,
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import MyProfile from '../screens/MyProfile';
+import OrderHistory from '../screens/OrderHistory';
+import {Text} from 'react-native-svg';
+// import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
-import {StatusBar} from 'react-native';
+type TabParamList = {
+  Home: undefined;
+  OrderHistory: undefined;
+  MyProfile: undefined;
+};
 
 export type RootParamList = {
+  Tabs: NavigatorScreenParams<TabParamList>;
   getStarted: undefined;
   Home: undefined;
   signUp: undefined;
@@ -28,6 +47,46 @@ export type RootParamList = {
 };
 
 const Root = createStackNavigator<RootParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
+export type TabNavigation = NavigationProp<TabParamList>;
+
+const TabBar = (props: BottomTabBarProps) => {
+  const {keyboardShown} = useKeyboard();
+  return keyboardShown && Platform.OS === 'android' ? null : (
+    <View>
+      <BottomTabBar {...props} />
+    </View>
+  );
+};
+
+export const Label =
+  (label: string) =>
+  ({focused}: {focused: boolean}) =>
+    <Text>{label}</Text>;
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      // eslint-disable-next-line react/no-unstable-nested-components
+      tabBar={props => <TabBar {...props} />}>
+      <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Tab.Screen
+        name="OrderHistory"
+        component={OrderHistory}
+        options={{
+          tabBarIcon: ({focused}) => <View />,
+          tabBarLabel: 'Order history',
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="MyProfile"
+        component={MyProfile}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export type RootNavigation = NavigationProp<RootParamList>;
 
@@ -41,7 +100,7 @@ export default () => {
     <>
       <StatusBar animated={true} backgroundColor={'black'} />
       <Root.Navigator
-        initialRouteName="getStarted"
+        initialRouteName="Tabs"
         screenOptions={{
           headerTitleAlign: 'center',
           headerTitleStyle: {
@@ -49,8 +108,8 @@ export default () => {
           },
         }}>
         <Root.Screen
-          name="Home"
-          component={Home}
+          name="Tabs"
+          component={Tabs}
           options={{headerShown: false}}
         />
         <Root.Screen
@@ -71,11 +130,11 @@ export default () => {
           options={{headerShown: false}}
         />
 
-        <Root.Screen
+        {/* <Root.Screen
           name="myProfile"
           component={myProfile}
           options={{headerShown: false}}
-        />
+        /> */}
 
         <Root.Screen
           name="details"
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,57 @@
 // import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Text,
+  Image,
+} from 'react-native';
+import Bars from '../../images/iconBars.svg';
+import Notification from '../../images/iconBell.svg';
+import HomeIcon from '../../images/Home.svg';
+import Orders from '../../images/PurchaseOrder.svg';
+import User from '../../images/User.svg';
 
 export default function Home() {
-  //   const navigation = useNavigation();
-  //   navigation.navigate('Home')
+  // const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.layout}>
+      <View style={styles.barsHeader}>
+        <View>
+          <Bars />
+        </View>
+        <View>
+          <Notification />
+        </View>
+      </View>
       <View>
-        <Image source={require('../../images/🦆 icon _Bars_.svg')} />
-        <Image source={require('../../images/iconBars.svg')} />
+        <TextInput style={styles.input} placeholder="Search for medicine" />
+      </View>
+      <View style={styles.categories}>
+        <Text>Categories</Text>
+        <View style={styles.slide} />
+      </View>
+      <View>
+        <View>
+          <Text>Featured products</Text>
+          <Text>See all</Text>
+        </View>
+        <View>
+          <View>
+            <Image source={require('../../images/Probiotic.png')} />
+          </View>
+          <View>
+            <View>
+              <Text>Probiotic</Text>
+              <Text>$50</Text>
+            </View>
+            <View>
+              <Image />
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -21,5 +63,54 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingHorizontal: 30,
     alignItems: 'center',
+  },
+  barsHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 360,
+    flexDirection: 'row',
+  },
+  input: {
+    height: 45,
+    width: 330,
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: '#ABEBF2',
+    marginTop: 20,
+  },
+  categories: {
+    marginTop: 20,
+  },
+  slide: {
+    backgroundColor: '#E8F3F1',
+    width: 360,
+    height: 150,
+    marginTop: 10,
+    borderRadius: 15,
+  },
+  nav: {
+    height: 70,
+    width: '100%',
+    backgroundColor: '#E8F3F1',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    alignItems: 'center',
+  },
+  bottom: {
+    justifyContent: 'space-evenly',
+    width: '100%',
+    height: 100,
+    flexDirection: 'row',
+  },
+  bottomIcons: {
+    alignItems: 'center',
+  },
+  bottomText: {
+    fontSize: 12,
+  },
+  navIcons: {
+    height: 200,
+    width: 200,
   },
 });
