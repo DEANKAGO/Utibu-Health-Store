c8c766a7fa951e7021edd97d21475232112a8828
deankago <martokhago@gmail.com>
2024-04-03 06:00:45 +0300

    create cart product increase function

--- src/context/PharmacyContext.tsx
+++ src/context/PharmacyContext.tsx
@@ -1,17 +1,55 @@
-import {createContext, ReactNode, useContext} from 'react';
-
-const PharmacyContext = createContext({});
+import {createContext, ReactNode, useContext, useState} from 'react';
 
 type PharmacyCartProps = {
   children: ReactNode;
 };
+
+type PharmacyContext = {
+  increaseProductCart: (id: number) => void;
+  //   decreaseProductCart: (id: number) => void;
+  //   removeProduct: (id: number) => void;
+  //   productQuantity: (id: number) => number;
+};
+
+type CartProduct = {
+  id: number;
+  quantity: number;
+};
+
+const PharmacyContext = createContext({} as PharmacyContext);
 
 export function usePharmacyContext() {
   return useContext(PharmacyContext);
 }
 
 export function PharmacyCart({children}: PharmacyCartProps) {
+  const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
+
+  function increaseProductCart(id: number) {
+    setCartProducts(availableProducts => {
+      if (availableProducts.find(product => product.id === id) === null) {
+        return [...availableProducts, {id, quantity: 1}];
+      } else {
+        return availableProducts.map(product => {
+          if (product.id === id) {
+            return {...product, quantity: product.quantity + 1};
+          } else {
+            return product;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <PharmacyContext.Provider value={{}}>{children}</PharmacyContext.Provider>
+    <PharmacyContext.Provider
+      value={{
+        increaseProductCart,
+        // decreaseProductCart,
+        // removeProduct,
+        // productQuantity,
+      }}>
+      {children}
+    </PharmacyContext.Provider>
   );
 }
